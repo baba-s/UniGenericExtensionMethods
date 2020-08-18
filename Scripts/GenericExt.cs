2498d6a0f4c3f032691a2a3c32aff83f42096640
baba_s <shotababa1206@gmail.com>
2020-08-18 16:50:24 +0900

    first commit

---
+++ Scripts/GenericExt.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kogane
+{
+	/// <summary>
+	/// ジェネリックの拡張メソッドを管理するクラス
+	/// </summary>
+	public static class GenericExt
+	{
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// 指定したオブジェクトが 1 つでもこのコレクションに含まれる場合 true を返します
+		/// </summary>
+		public static bool EqualsAny<T>( this T self, IEnumerable<T> values )
+		{
+			foreach ( var n in values )
+			{
+				if ( self.Equals( n ) )
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// 指定したオブジェクトが 1 つでもこのコレクションに含まれる場合 true を返します
+		/// </summary>
+		public static bool EqualsAny<T>( this T self, IReadOnlyList<T> values )
+		{
+			for ( var i = 0; i < values.Count; i++ )
+			{
+				var n = values[ i ];
+				if ( self.Equals( n ) )
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// 指定したオブジェクトが 1 つでもこのコレクションに含まれる場合 true を返します
+		/// </summary>
+		public static bool EqualsAny<T>( this T self, params T[] values )
+		{
+			for ( var i = 0; i < values.Length; i++ )
+			{
+				var n = values[ i ];
+				if ( self.Equals( n ) )
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// 指定したオブジェクトが 1 つでもこのコレクションに含まれる場合 true を返します
+		/// </summary>
+		[Obsolete( "代わりに EqualsAny を使用してください" )]
+		public static bool ContainsAny<T>( this T self, IEnumerable<T> values )
+		{
+			return self.EqualsAny( values );
+		}
+
+		/// <summary>
+		/// 指定したオブジェクトが 1 つでもこのコレクションに含まれる場合 true を返します
+		/// </summary>
+		[Obsolete( "代わりに EqualsAny を使用してください" )]
+		public static bool ContainsAny<T>( this T self, IReadOnlyList<T> values )
+		{
+			return self.EqualsAny( values );
+		}
+
+		/// <summary>
+		/// 指定したオブジェクトが 1 つでもこのコレクションに含まれる場合 true を返します
+		/// </summary>
+		[Obsolete( "代わりに EqualsAny を使用してください" )]
+		public static bool ContainsAny<T>( this T self, params T[] values )
+		{
+			return self.EqualsAny( values );
+		}
+	}
+}
